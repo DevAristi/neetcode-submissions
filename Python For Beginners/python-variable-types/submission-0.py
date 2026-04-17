86ea7c8999f47a020008ccce58bbe6b76f8b3598
Sebastián Aristi <sebaristi09@gmail.com>
2026-04-16 20:00:37 -0600

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 9
+float_type = 9.333
+bool_type = False
+string_type = "Damn Son"
+list_type = [6,7]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
