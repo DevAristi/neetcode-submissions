300163067eff854663394c6ecacbb9ca2f4ecfee
Sebastián Aristi <sebaristi09@gmail.com>
2026-08-19 06:33:31 -0600

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 912
+print(type(variable))
+variable = 98.57
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "GGS BOY"
+print(type(variable))
+variable = [1,2,3,4,5,6]
+print(type(variable))
